e4337d0eafef69253b5dd6aa73b5a092049d2db8
Brian Routzong <brianroutzong@pop-os.localdomain>
2020-07-21 08:20:49 -0500

    Committer: Brian Routzong <brianroutzong@pop-os.localdomain>

--- server/server.js
+++ server/server.js
@@ -6,10 +6,10 @@ const appMiddleware = require('./middleware/middleware');
 
 const PORT = process.env.PORT || 3006;
 const app = express();
-
+app.use(requireHTTPS);
 appMiddleware(app);
 routes(app);
-app.use(requireHTTPS);
+
 app.get('/', (req, res) => {
   return res.status(200).json({message: 'connected',});
 });

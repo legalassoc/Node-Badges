55a9f07b80d15fe4852fac904b2ac29b6ad95906
Brian Routzong <brianroutzong@pop-os.localdomain>
2020-07-21 22:32:00 -0500

    Committer: Brian Routzong <brianroutzong@pop-os.localdomain>

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const appMiddleware = require('./middleware/middleware');
 
 const PORT = process.env.PORT || 3006;
 const app = express();
-app.use(requireHTTPS);
 appMiddleware(app);
 routes(app);
 
@@ -17,11 +16,3 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`😎 Server is listening on port ${PORT}`);
 });
-
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
